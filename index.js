dccfb7a68557574e8a9731bacc36db2dee3a16f9
EngeReyes23 <engelreyes23@gmail.com>
2022-04-04 17:16:46 -0600

    configurando Express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello world from node js");
+const express = require("express");
+
+// Inicializa express
+const app = express();
+
+// Establece el puerto
+app.listen(4000, () => {
+	console.log(`Server on port ${4000}`);
+});
+
+// Ruta principal
+app.get("/", (req, res) => {
+	console.log("Petición GET a la ruta /");
+	res.json({
+		message: "Hola mundo",
+	});
+});
