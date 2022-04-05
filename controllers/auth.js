8b3cc52636698da39ca5d26e6506c35a8ef15a9a
EngeReyes23 <engelreyes23@gmail.com>
2022-04-04 19:43:07 -0600

    custom middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,7 @@
 const { response } = require("express");
-const { validationResult } = require("express-validator");
 
 // Login
 const login = (req, res = response) => {
-	const errors = validationResult(req);
-
-	if (!errors.isEmpty()) {
-		return res.status(400).json({
-			ok: false,
-			errors: errors.mapped(),
-		});
-	}
-
 	res.json({
 		message: "Login OK",
 	});
@@ -20,15 +10,6 @@ const login = (req, res = response) => {
 // Register
 const register = (req, res = response) => {
 	const { name, email } = req.body;
-
-	const results = validationResult(req);
-
-	if (!results.isEmpty()) {
-		return res.status(400).json({
-			ok: false,
-			errors: results.mapped(),
-		});
-	}
 
 	res.status(201).json({
 		ok: true,
--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ const { Router } = require("express");
 const { check } = require("express-validator");
 //
 const { login, register, renewToken } = require("../controllers/auth");
+const { validarCampos } = require("../middlewares/validarCampos");
 
 const router = Router();
 
@@ -17,6 +18,7 @@ router.post(
 		// middleware
 		check("email", "El email es obligatorio").isEmail(),
 		check("password", "El password es obligatorio").not().isEmpty(),
+		validarCampos,
 	],
 	login
 );
@@ -31,6 +33,7 @@ router.post(
 		check("password", "El password debe tener mínimo 6 caracteres").isLength({
 			min: 6,
 		}),
+		validarCampos,
 	],
 	register
 );
