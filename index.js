f41dedc427d3dc8786ca504f91735db5fb050793
EngeReyes23 <engelreyes23@gmail.com>
2022-04-05 22:36:09 -0600

    cors agregados

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { dbConnection } = require('./database/config');
+const cors = require('cors');
 require('dotenv').config();
 
 // Inicia express
@@ -7,6 +8,9 @@ const app = express();
 
 // DB
 dbConnection();
+
+// CORS
+app.use(cors());
 
 // Establece el puerto
 app.listen(process.env.PORT, () => {
