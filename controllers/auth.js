5d32d38ec04d2eb837bd8e88971d7ee722f79c5d
EngeReyes23 <engelreyes23@gmail.com>
2022-04-05 20:46:15 -0600

    encriptando password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,58 +1,63 @@
-const { response } = require("express");
-const User = require("../models/User");
+const { response } = require('express');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
 
 // Login
 const login = (req, res = response) => {
-	res.json({
-		message: "Login OK",
-	});
+  res.json({
+    message: 'Login OK',
+  });
 };
 
 // Register
 const register = async (req, res = response) => {
-	const { email } = req.body;
+  const { email } = req.body;
 
-	try {
-		// Busca el usuario
-		let user = await User.findOne({ email });
+  try {
+    // Busca el usuario
+    let user = await User.findOne({ email });
 
-		// Si el usuario existe
-		if (user) {
-			return res.status(400).json({
-				ok: false,
-				msg: "Ya existe un usuario con ese email",
-			});
-		}
+    // Si el usuario existe
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Ya existe un usuario con ese email',
+      });
+    }
 
-		user = new User(req.body); // Crea el usuario
+    user = new User(req.body); // Crea el usuario
 
-		await user.save(); // Guarda el usuario
+    // Encripta la contraseña
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(user.password, salt);
 
-		res.status(201).json({
-			ok: true,
-			uid: user.id,
-			name: user.name,
-		});
-	} catch (error) {
-		res.status(500).json({
-			ok: false,
-			message: "Error in the register",
-			error,
-		});
-		new Error("Error creating user");
-	}
+    await user.save(); // Guarda el usuario
+
+    res.status(201).json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      message: 'Error in the register',
+      error,
+    });
+    new Error('Error creating user');
+  }
 };
 
 // Renew token
 const renewToken = (req, res = response) => {
-	res.json({
-		message: "Renew token OK",
-	});
+  res.json({
+    message: 'Renew token OK',
+  });
 };
 
 // Export
 module.exports = {
-	login,
-	register,
-	renewToken,
+  login,
+  register,
+  renewToken,
 };
